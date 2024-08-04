a80074d880466ef62c5f4720ad6dab2bbb8d0872
muni <ed23s300@smail.iitm.ac.in>
2024-08-04 11:27:09 +0530

    feat: read messga from left and right camera topics, detect the objects and show the detections         as BBoxes in Image

--- scripts/detections.py
+++ scripts/detections.py
@@ -35,6 +35,48 @@ from std_msgs.msg import String
 
 # Miscelleneaous
 import time
+import cProfile
+
+
+def draw_bbox(input_img, detections):
+    """
+        input : Raw Image, YOLO Detection results
+        outupt : Rawimage with boundning boxes resulted from the YOLO detections
+        Info :
+            This function takes the input as detections of YOLO and Raw image, draws bounding boxes onto the image and returns it
+    """
+    for result in detections:
+        # print(result)
+        boxes = result.boxes.cpu().numpy()      # stores the detection summary
+        classes = result.names                  # stores the dictionary of classes
+        # print("BOXES : ", boxes)
+        # print("CLASSES : ", classes)
+
+        for box in boxes:
+            # print("\n\n\nBOX : ", box)
+            # print("BOx XYXY : ", type(box.xyxy), box.xyxy.shape)
+            x1, y1, x2, y2 = box.xyxy.reshape(-1) # rehape array of type (1,4) to (4,)
+            # print(f"Co ordinates : {x1}, {y1}, {x2}, {y2}")
+            # print(type(x1), type(x2), type(y1), type(y2))
+            confidence = box.conf.item()
+            # print("Confidence : ", confidence)
+            class_id = box.cls.item()
+            label = f"{classes[class_id]} {confidence:.2f}"
+
+                # Draw bounding box and label
+            color = (0, 255, 0)
+            font = cv2.FONT_HERSHEY_SIMPLEX
+
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2),int(y2)
+
+            # draw a rectangle and visuzlaise it
+            cv2.rectangle(input_img, ( x1, y1 ), ( x2, y2 ), color, 1)
+            
+            cv2.putText(input_img, label, (x1, y1 - 10), font, 0.5, color, thickness=1, lineType=cv2.LINE_AA)
+
+    return input_img
+
+
 
 def bag_read():
     # Initialize the ROS node with the name 'talker'
@@ -60,11 +102,11 @@ def bag_read():
         exit(-1)
     # ================================================================================================
     # define the YOLO model
-    obj_classification_cam_model = YOLO("yolov5n.pt")
+    obj_classification_cam_model = YOLO("yolov5nu.pt")
     # ================================================================================================
     i = 0.
     # read data from the ROSBag
-    for topic, msg, ts in bag.read_messages(topics = ['/right_cam/image_rect_color/compressed']):
+    for topic, msg, ts in bag.read_messages(topics = []):
         print(type(msg), topic, ts)
         i += 1
 
@@ -86,39 +128,10 @@ def bag_read():
             right_cam_results = obj_classification_cam_model(raw_img)
             # for r in right_cam_results:
             #     print(f"Detected {len(r)} objects in image")
-
-            for result in right_cam_results:
-                # print(result)
-                boxes = result.boxes.cpu().numpy()      # stores the detection summary
-                classes = result.names                  # stores the dictionary of classes
-                # print("BOXES : ", boxes)
-                # print("CLASSES : ", classes)
-
-                for box in boxes:
-                    # print("\n\n\nBOX : ", box)
-                    # print("BOx XYXY : ", type(box.xyxy), box.xyxy.shape)
-                    x1, y1, x2, y2 = box.xyxy.reshape(-1) # rehape array of type (1,4) to (4,)
-                    # print(f"Co ordinates : {x1}, {y1}, {x2}, {y2}")
-                    # print(type(x1), type(x2), type(y1), type(y2))
-                    confidence = box.conf.item()
-                    # print("Confidence : ", confidence)
-                    class_id = box.cls.item()
-                    label = f"{classes[class_id]} {confidence:.2f}"
-
-                     # Draw bounding box and label
-                    color = (0, 255, 0)
-                    font = cv2.FONT_HERSHEY_SIMPLEX
-
-                    x1, y1, x2, y2 = int(x1), int(y1), int(x2),int(y2)
-
-                    # draw a rectangle and visuzlaise it
-                    cv2.rectangle(raw_img, ( x1, y1 ), ( x2, y2 ), color, 1)
-                    
-                    cv2.putText(raw_img, label, (x1, y1 - 10), font, 0.5, color, thickness=1, lineType=cv2.LINE_AA)
-
-               
+            raw_img = draw_bbox(raw_img, right_cam_results)
+            
             # Display the image with bounding boxes
-            cv2.imshow("YOLO Inference", raw_img)
+            cv2.imshow("YOLO Right Inference", raw_img)
             cv2.waitKey(100)  # Wait for 1 ms for the window to update
 
             # print(f"Right camera detections : {right_cam_results}")
@@ -127,20 +140,41 @@ def bag_read():
 
             # rt_cam_img.publish(msg)
         # break
-'''
+
+
+        if (topic == "/left_cam/image_rect_color/compressed"):
+            # decode the image using opencv and dispay it
+            msg_np = np.frombuffer(msg.data, dtype='uint8')
+            
+            raw_img = cv2.imdecode(msg_np,cv2.IMREAD_COLOR)
+
+            # perform classification
+            left_cam_results = obj_classification_cam_model(raw_img)
+          
+            raw_img = draw_bbox(raw_img, left_cam_results)
+            
+            # Display the image with bounding boxes
+            cv2.imshow("YOLO left Inference", raw_img)
+            cv2.waitKey(100)  # Wait for 100 ms for the window to update
+
+
         if(i%4 == 0):
             # process one frame of all topics and sleep
             time.sleep(0.5)
             print("===================================================================")
-        if (i==4*20):# process one frame and sleep):
+        if (i==4*20):# process 20 frames
             break
 
-'''
+        if (topic == "/dominant_obj_info"):
+            continue
 
-        
+        if (topic == "/velodyne_points"):
+            continue
+
 
 if __name__ == '__main__':
     try:
         bag_read()
+        # cProfile.run('bag_read()')
     except rospy.ROSInterruptException:
         pass
